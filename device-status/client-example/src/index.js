f29cb976c3340a518f2e23d7dc74dcfed4d221c2
shusuke0812 <shu.ota0812@gmail.com>
2023-11-09 22:38:53 +0900

    add: apollo client

--- device-status/client-example/src/index.js
+++ device-status/client-example/src/index.js
@@ -3,12 +3,25 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import ApolloClient, { gql } from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+
+const client = new ApolloClient({ uri: 'http://localhost:4000/graphql' })
+const query = gql`
+    {
+        totalUsers
+        totalPhotos
+    }
+`
+client.query({query})
+    .then(({data}) => console.log('data=', data))
+    .catch(console.error)
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <ApolloProvider client={client}>
+        <App />
+    </ApolloProvider>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
