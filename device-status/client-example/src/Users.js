9b5c7840e57de9564c660bb3c770eabddadd2535
shusuke0812 <shu.ota0812@gmail.com>
2023-11-11 23:32:08 +0900

    add: displaying fake user

--- device-status/client-example/src/Users.js
+++ device-status/client-example/src/Users.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import { Query } from 'react-apollo'
+import { Query, Mutation } from 'react-apollo'
+import { gql } from 'apollo-boost'
 import { ROOT_QUERY } from './App'
 
 const Users = () =>
@@ -18,6 +19,11 @@ const UserList = ({ count, users, refetchUsers }) =>
     <div>
         <p>{count} Users</p>
         <button onClick={() => refetchUsers()}>Refetch Users</button>
+        <Mutation mutation={ADD_FAKE_USERS_MUTATION} variables={{ count: 1 }}>
+            {addFakeUsers =>
+                <button onClick={addFakeUsers}>Add Fake Users</button>
+            }
+        </Mutation>
         <ul>
             {users.map(user =>
                 <UserListItem
@@ -32,5 +38,15 @@ const UserListItem = ({ name, avatar }) =>
     <li>
         <img src={avatar} width={52} height={52} alt="" />
     </li>
+
+const ADD_FAKE_USERS_MUTATION = gql`
+    mutation addFakeUsers($count:Int!) {
+        addFakeUsers(count:$count) {
+            githubLogin
+            name
+            avatar
+        }
+    }
+`
 
 export default Users
