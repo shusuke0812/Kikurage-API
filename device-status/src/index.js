af7bd799c7879ef6b082666e08b9adc8abe706b4
shusuke0812 <shu.ota0812@gmail.com>
2024-01-13 19:43:32 +0900

    fix: httpServerr

--- device-status/src/index.js
+++ device-status/src/index.js
@@ -1,3 +1,4 @@
+const { createServer } = require('http')
 const { ApolloServer } = require(`apollo-server-express`)
 const express = require(`express`)
 const expressPlayground = require(`graphql-playground-middleware-express`).default
@@ -8,6 +9,7 @@ require(`dotenv`).config()
 
 const typeDefs = readFileSync(`./typeDefs.graphql`, `UTF-8`)
 const resolvers = require(`./resolvers`)
+const { create } = require('domain')
 
 async function start() {
     const app = express()
@@ -33,10 +35,15 @@ async function start() {
     server.applyMiddleware({ app })
     app.get(`/`, (req, res) => res.end(`Welcom to the PhotoShare API`))
     app.get(`/playground`, expressPlayground({ endpoint: `/graphql` }))
-    app
-      .listen({ port: 4000 }, () =>
-        console.log(`GraphQL Server running @ http://localhost:4000${server.graphqlPath}`)
-      )
+    
+    
+    const httpServer = createServer(app)
+    server.installSubscriptionHandlers(httpServer)
+    httpServer.timeout = 5000
+
+    httpServer.listen({ port: 4000 }, () =>
+        console.log(`GraphQL Serverr running at localhost:4000${server.graphqlPath}`)
+    )
 }
 
 start()
