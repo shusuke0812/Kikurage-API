89fef371a5806b9de88b9f44cf734fcedf44c6a9
shusuke0812 <shu.ota0812@gmail.com>
2024-04-13 21:55:09 +0900

    change: get device status endpoint

--- device-status/src/main/kotlin/com/shusuke/kikurage/devicestatus/presentation/controller/DeviceStatusController.kt
+++ device-status/src/main/kotlin/com/shusuke/kikurage/devicestatus/presentation/controller/DeviceStatusController.kt
@@ -5,12 +5,12 @@ import com.shusuke.kikurage.devicestatus.presentation.form.GetDeviceStatusRespon
 import org.springframework.web.bind.annotation.*
 
 @RestController
-@RequestMapping("/{device_id}/status")
+@RequestMapping("device")
 @CrossOrigin(origins = ["http://localhost:8003"], allowCredentials = "true")
 class DeviceStatusController(
     private val deviceStatusService: DeviceStatusService
 ) {
-    @GetMapping
+    @GetMapping("/status/{device_id}")
     fun getDeviceStatus(@PathVariable("device_id") deviceId: Long): GetDeviceStatusResponse {
         val status = deviceStatusService.getDeviceStatus(deviceId)
         return GetDeviceStatusResponse(
