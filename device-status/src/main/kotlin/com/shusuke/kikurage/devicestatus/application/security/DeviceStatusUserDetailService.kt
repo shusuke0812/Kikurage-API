6a990a272abfc026a7725308d73c0af9215e30e5
shusuke0812 <shu.ota0812@gmail.com>
2024-04-21 15:26:17 +0900

    add: DeviceStatusUserDetailService

---
+++ device-status/src/main/kotlin/com/shusuke/kikurage/devicestatus/application/security/DeviceStatusUserDetailService.kt
@@ -0,0 +1,59 @@
+package com.shusuke.kikurage.devicestatus.application.security
+
+import com.shusuke.kikurage.devicestatus.application.AuthenticationService
+import com.shusuke.kikurage.devicestatus.domain.RoleType
+import com.shusuke.kikurage.devicestatus.domain.model.User
+import org.springframework.security.core.GrantedAuthority
+import org.springframework.security.core.authority.AuthorityUtils
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.security.core.userdetails.UserDetailsService
+import java.util.*
+
+class DeviceStatusUserDetailService(
+    private val authenticationService: AuthenticationService
+) : UserDetailsService {
+    override fun loadUserByUsername(username: String): UserDetails? {
+        val user = authenticationService.findUser(username)
+        return user?.let { DeviceStatusUserDetails(user) }
+    }
+}
+
+data class DeviceStatusUserDetails(
+    val id: Long,
+    val deviceId: Long,
+    val email: String,
+    val password: String,
+    val roleType: RoleType,
+    val createdAt: Date,
+    val updatedAt: Date
+) : UserDetails {
+    constructor(user: User) : this(user.id, user.deviceId, user.email, user.password, user.roleType, user.createdAt, user.updatedAt)
+
+    override fun getAuthorities(): MutableCollection<out GrantedAuthority> {
+        return AuthorityUtils.createAuthorityList(this.roleType.toString())
+    }
+
+    override fun isEnabled(): Boolean {
+        return true
+    }
+
+    override fun getUsername(): String {
+        return this.email
+    }
+
+    override fun isCredentialsNonExpired(): Boolean {
+        return true
+    }
+
+    override fun getPassword(): String {
+        return this.password
+    }
+
+    override fun isAccountNonExpired(): Boolean {
+        return true
+    }
+
+    override fun isAccountNonLocked(): Boolean {
+        return true
+    }
+}
